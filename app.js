2939202211334c6c2ff2ba18b66d62426270cd2d
JuanJosePG <jjgp.code@gmail.com>
2021-01-18 18:29:21 +0100

    feature/javascript: some irrelevant fixes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 function initMap() {
     // The location of AIRLid viajes
     const airlid = { lat: 36.64549536159755, lng: -4.6815955886947584 };
-    // The map, centered at Uluru
+
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: airlid,
     });
-    // The marker, positioned at Uluru
+
     const marker = new google.maps.Marker({
         position: airlid,
         map: map,
